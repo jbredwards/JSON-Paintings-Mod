72da294db5c2cd074fc23b9a51fb322f9e27237d
jbred <47703524+jbredwards@users.noreply.github.com>
2026-08-20 00:08:13 -0400

    Update PaintingUpdateEvent.java

--- src/main/java/git/jbredwards/jsonpaintings/api/event/PaintingUpdateEvent.java
+++ src/main/java/git/jbredwards/jsonpaintings/api/event/PaintingUpdateEvent.java
@@ -5,9 +5,12 @@
 
 package git.jbredwards.jsonpaintings.api.event;
 
+import git.jbredwards.jsonpaintings.api.ActivePaintingInfo;
+import git.jbredwards.jsonpaintings.api.PaintingInfo;
 import net.minecraft.entity.item.EntityPainting;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import org.jetbrains.annotations.ApiStatus;
 
 import javax.annotation.Nonnull;
 
@@ -23,29 +26,72 @@ import javax.annotation.Nonnull;
  * <br>
  * This event is fired on the {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS}.
  *
- * @since 1.3.0
  * @author jbred
  *
  */
+@ApiStatus.AvailableSince("1.3.0")
 @Cancelable
 public class PaintingUpdateEvent extends EntityEvent
 {
-    @Nonnull
-    public final EntityPainting painting;
-    public PaintingUpdateEvent(@Nonnull EntityPainting paintingIn) {
+    /**
+     * The painting entity being ticked.
+     */
+    @ApiStatus.AvailableSince("1.3.0")
+    @Nonnull public final EntityPainting painting;
+
+    @ApiStatus.AvailableSince("1.3.0")
+    public PaintingUpdateEvent(@Nonnull final EntityPainting paintingIn) {
         super(paintingIn);
-        painting = paintingIn;
+        this.painting = paintingIn;
     }
 
+    /**
+     * @return The painting entity being ticked.
+     * @author jbred
+     */
+    @ApiStatus.AvailableSince("1.3.0")
     @Nonnull
-    public EntityPainting getPainting() { return painting; }
-
-    @Nonnull
-    public EntityPainting.EnumArt getArt() { return getPainting().art; }
+    public EntityPainting getPainting() {
+        return this.painting;
+    }
 
     /**
-     * @param mantle the mantle to compare against.
-     * @return true if this painting's art mantle is equal to the one provided.
+     * @return Type of the painting entity being ticked.
+     * @author jbred
      */
-    public boolean matchesArt(@Nonnull final String mantle) { return mantle.equalsIgnoreCase(getArt().title); }
+    @ApiStatus.AvailableSince("1.3.0")
+    @Nonnull
+    public EntityPainting.EnumArt getArt() {
+        return this.getPainting().art;
+    }
+
+    /**
+     * @return JSON Paintings data of the painting entity being ticked.
+     * @author jbred
+     */
+    @ApiStatus.AvailableSince("1.5.0")
+    @Nonnull
+    public PaintingInfo getExtras() {
+        return ActivePaintingInfo.get(this.getArt());
+    }
+
+    /**
+     * Note: This compares the mantle, not the {@link PaintingInfo#title title}.
+     * @param mantle The mantle to compare against.
+     * @return True if this painting's art mantle is equal to the one provided.
+     */
+    @ApiStatus.AvailableSince("1.5.0")
+    public boolean matches(@Nonnull final String mantle) {
+        return mantle.equals(this.getArt().title);
+    }
+
+    /**
+     * Use {@link PaintingUpdateEvent#matches(String)} instead.
+     * <br> This method uses an incorrect calculation, painting mantles are case-sensitive.
+     */
+    @ApiStatus.AvailableSince("1.3.0")
+    @Deprecated
+    public boolean matchesArt(@Nonnull final String mantle) {
+        return mantle.equalsIgnoreCase(this.getArt().title);
+    }
 }
